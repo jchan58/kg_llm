15552238d50578ea232cbe7abfc100c0c266be45
Er <ercj@ncbi.nlm.nih.gov>
2025-11-20 21:12:47 -0600

    added in glioblastoma

--- upload_jsonl.py
+++ upload_jsonl.py
@@ -6,7 +6,7 @@ MONGO_URI = st.secrets["MONGO_URI"]
 client = MongoClient(MONGO_URI)
 db = client["kgxllm"]
 collection = db["diseases"]
-jsonl_file = "drug_results\pancreaticcancer.merged.jsonl"
+jsonl_file = "drug_results\glioblastoma.merged.jsonl"
 
 drug_map = {}
 with open(jsonl_file, "r", encoding="utf-8") as file:
@@ -19,14 +19,14 @@ with open(jsonl_file, "r", encoding="utf-8") as file:
         drug_name = object.get("drug")
         drug_map[drug_name] = object 
 
-pancreaticcancer_doc = {
+glioblastoma_doc = {
     "disease": object['disease'].strip().lower(), 
     "drug_map": drug_map
 }
 
 collection.update_one(
-    {"disease": "pancreaticcancer"},
-    {"$set": pancreaticcancer_doc},
+    {"disease": "glioblastoma"},
+    {"$set": glioblastoma_doc},
     upsert=True
 )
-print(f"Uploaded pancreatic cancer with {len(drug_map)} drugs.")
+print(f"Uploaded glioblastoma with {len(drug_map)} drugs.")
