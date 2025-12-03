17aea87cdc0e49be9a26db32f8b26baf5e5d05ea
Er <ercj@ncbi.nlm.nih.gov>
2025-12-02 18:00:08 -0600

    fixed

--- annotation_shared.py
+++ annotation_shared.py
@@ -130,6 +130,12 @@ def run_annotation(assigned_disease):
     if prev_Q1 == "No":
         prev_Q1 = "No — No clinical trials identified for this drug in this disease"
 
+    st.markdown(
+    "<div style='margin-top:-10px; font-size:0.9rem; color:#666;'><em>"
+    "(If, and only if, you select “No,” please proceed to Q2. For all other selections, please go directly to Q4.)"
+    "</em></div>",
+    unsafe_allow_html=True
+)
     Q1_value = st.radio(
         "Q1. What is the latest status of this drug for this disease? (single choice)",
         Q1_options,
